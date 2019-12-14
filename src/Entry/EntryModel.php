da34137c7cdc8307bf1c4833aeca4c9d94524bf5
Robin Radic <rradic@hotmail.com>
2019-12-15 00:16:44 +0100

    upd

--- src/Entry/EntryModel.php
+++ src/Entry/EntryModel.php
@@ -2,7 +2,11 @@
 
 namespace Pyro\Platform\Entry;
 
+use Pyro\ActivityLogModule\Activity\Traits\CausesActivity;
+use Pyro\ActivityLogModule\Activity\Traits\LogsActivity;
+
 class EntryModel extends \Anomaly\Streams\Platform\Entry\EntryModel
 {
-
+use LogsActivity;
+use CausesActivity;
 }
